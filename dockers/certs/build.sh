4d887f0ad47e88cb019d75e8ac46c1a3ebbb2e09
veto <veto@veto.dev>
2026-07-15 15:24:19 +0700

    up: dockers/certs/build.sh dockers/certs/grid.local/all.pem dockers/certs/grid.local/cert.pem dockers/certs/grid.local/key.pem dockers/certs/minica dockers/certs/minica-key.pem dockers/certs/minica.pem

--- dockers/certs/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-domain=grid.local
-rm $domain  -Rf
-./minica --domains $domain
-cat ./$domain/cert.pem ./$domain/key.pem > ./$domain/all.pem
-cp ./$domain/* ../etc/lighttpd/conf-enabled/certs/ -Rf
